9c018f2889cf9a36eaa44228b33c7297736e8e33
gabrielaPLMPRO <gabriela@plmpro.com.br>
2025-04-10 20:34:54 -0300

    inserindo routes.dart

---
+++ recipes/lib/routes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppRoutes {
+  static const listRecipes = '/';
+  static const viewRecipe = '/view-recipe';
+  static const formRecipe = '/form-recipe';
+  static const formIngredient = '/form-ingredient';
+  static const formStep = '/form-step';
+}
